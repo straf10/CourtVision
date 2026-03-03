255fa1af137878bbaea1b6a604857653f593e3dd
Nikos Strafiotis <nstrafio@csd.auth.gr>
2026-03-03 14:32:39 +0200

    Added Zero shot classification model that now assigns players teams based on jersey color

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
 from drawers import PlayerTracksDrawer, BallTracksDrawer
+from team_assigner import TeamAssigner
 
 INPUT_VIDEO = "input_videos/video_1.mp4"
 PLAYER_MODEL = "models/best.pt"
@@ -28,10 +29,15 @@ def main():
     ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
     ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
 
+    # Team Assignment
+    team_assigner = TeamAssigner()
+    player_assignment = team_assigner.get_player_teams_across_frames(video_frames, player_tracks, read_from_stub=True, stub_path="stubs/player_teams.pkl")
+
+    # Draw Player Tracks
     player_tracks_drawer = PlayerTracksDrawer()
     ball_tracks_drawer = BallTracksDrawer()
 
-    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment)
     output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
 
     save_video(output_video_frames, OUTPUT_VIDEO, fps)
